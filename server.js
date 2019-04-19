5c0add86ee2e65445952dee77a03346652f83d6c
unknown <delgadojuan222@gmail.com>
2019-04-19 15:42:24 -0500

    mongouri

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var db = mongoose.connect("mongodb://localhost/cowboysscrape", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/cowboysscrape";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+var db = mongoose.connection;
 
 app.get("/", function (req, res) {
     Article.find({})
